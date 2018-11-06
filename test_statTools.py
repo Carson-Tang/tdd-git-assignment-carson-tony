d4ba32b69d590c4d0e87df610a5ed23449b7f222
Carson-Tang <carsonkstang@gmail.com>
2018-11-06 10:57:00 -0500

    add test for upperquartile

--- test_statTools.py
+++ test_statTools.py
@@ -6,6 +6,7 @@ list_data2 = [2, 9, 6, 2, 4, 7, 20, 1, 3, 4, 8, 18]
 list_data3 = [1, 2, 3]
 empty_data = []
 
+
 def test_mean_1():
     pass
 def test_mean_2():
@@ -13,12 +14,14 @@ def test_mean_2():
 def test_mean_3():
     pass
 
+
 def test_median_1():
     pass
 def test_median_2():
     pass
 def test_median_3():
     pass
+
 
 # Value frequencies - value:frequency
 # empty list, ans = None
@@ -33,12 +36,14 @@ def test_mode_2():
 def test_mode_3():
     assert(mode(list_data2) == [2, 4])
 
+
 def test_range_1():
     pass
 def test_range_2():
     pass
 def test_range_3():
     pass
+
 
 # sorted list = [0, 1, 2, 4, 4, 5, 6, 7, 8, 9, 20]
 # split = [0, 1, 2, 4, 4] [6, 7, 8, 9, 20]
@@ -54,12 +59,21 @@ def test_lowerquartile_2():
 def test_lowerquartile_3():
     assert(lower_quartile(empty_data) == None)
 
+
+# sorted list = [0, 1, 2, 4, 4, 5, 6, 7, 8, 9, 20]
+# split = [0, 1, 2, 4, 4] [6, 7, 8, 9, 20]
+# split top half = [6, 7] 8 [9, 20], ans = 8
 def test_upperquartile_1():
-    pass
+    assert(upper_quartile(list_data) == 8)
+# sorted list = [1, 2, 2, 3, 4, 4, 6, 7, 8, 9, 18, 20]
+# split = [1, 2, 2, 3, 4, 4] [6, 7, 8, 9, 18, 20]
+# split lower half = [6, 7] 8, 9 [18, 20], ans = (8+9)/2 = 8.5
 def test_upperquartile_2():
-    pass
+    assert(upper_quartile(list_data2) == 8.5)
+# list of length 3
 def test_upperquartile_3():
-    pass
+    assert(upper_quartile(list_data3) == 0)
+
 
 # sum = 66, elements = 11, mean = 6, ans = 296/11
 def test_variance_1():
@@ -70,6 +84,7 @@ def test_variance_2():
 # sum = 0, elements = 0, ans = None
 def test_variance_3():
     assert(variance(empty_data) == None)
+
 
 def test_standard_deviation_1():
     pass
