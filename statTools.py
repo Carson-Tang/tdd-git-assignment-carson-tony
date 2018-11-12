7d12a8253743e666fe737a82a2b156ca2eb1e9ae
Tony Ni <tony.zt.ni@gmail.com>
2018-11-12 10:20:44 -0500

    update from master

--- statTools.py
+++ statTools.py
@@ -1,18 +1,41 @@
+"""
+------------------------------------------------------------------------------------------------------------------------
+Name: statTools.py
+Purpose:
+    Library of Statistical Functions for Measures of Central Tendencies and Measures of Spread for
+    Includes : mean, median, mode, range, upper quartile, lower quartile, variance and standard deviation
+Author: Tang.C, Ni.T
+Created: 2018/11/11
+------------------------------------------------------------------------------------------------------------------------
+"""
 import math
+def not_int_list(list_data: list) -> bool:
+    ''' Determine if list contains non integer value
+    :param list_data: list of values
+    :return: bool, True if list contains non int, otherwise False
+    Author: Tang.C
+    '''
+    # Loop through list
+    for value in list_data:
+        # Return true if the value is not int
+        if not isinstance(value, int):
+            return True
+    return False
+
 def mean(list_data: list) -> float:
     ''' Returns mean of list
-
-        :param list_data: list of values
-        :return: float, mean of the list
-        '''
+    :param list_data: list of values
+    :return: float, mean of the list
+    Author: Ni.T
+    '''
     if len(list_data) == 0 : return -1
     return sum(list_data) / len(list_data)
 
 def median(list_data: list) -> float:
     ''' Returns median of list
-
     :param list_data: list of values
     :return: float, median of the list
+    Author: Ni.T
     '''
     if len(list_data) == 0 : return -1
     list_data.sort()
@@ -23,11 +46,6 @@ def median(list_data: list) -> float:
         return list_data[divider]
 
 def mode(list_data: list) -> list:
-    ''' Returns mode of list
-
-    :param list_data: list of values
-    :return: list, list of most frequent values
-    '''
     # List of values to return
     ret_mode = []
     # Handle exception if list_data is not a list
@@ -54,9 +72,9 @@ def mode(list_data: list) -> list:
 
 def range(list_data: list) -> float:
     ''' Returns range of list
-
     :param list_data: list of values
     :return: float, range of the list
+    Author: Ni.T
     '''
     if len(list_data) == 0 : return -1
     list_data.sort()
@@ -65,14 +83,17 @@ def range(list_data: list) -> float:
 def lower_quartile(list_data: list) -> int:
     ''' Returns lower quartile of a list
         Lower quartile : median of lower half of list
-
     :param list_data: list of values
     :return: int, lower quartile of list
+    Author: Tang.C
     '''
     # Handle non list input
     if not isinstance(list_data, list) : raise TypeError("Not list")
     # Handle list that is less than 4 values, no lower quartile, returns -1
     if len(list_data) < 4 : return -1
+    # Handle non int in list
+    if not_int_list(list_data) : raise ValueError("List contains non integer value")
+
     list_data.sort()
     # Create list consisting of lower half of list_data
     list_lowerhalf = list_data[:len(list_data)//2]
@@ -86,14 +107,17 @@ def lower_quartile(list_data: list) -> int:
 def upper_quartile(list_data: list) -> int:
     ''' Return upper quartile of a list
         Upper quartile : median of upper half of list
-
     :param list_data: list of values
     :return: int, upper quartile of list
+    Author: Tang.C
     '''
     # Handle non list input
     if not isinstance(list_data, list) : raise TypeError("Not list")
     # Handle list that is less than 4 values, no upper quartile, returns -1
     if len(list_data) < 4 : return -1
+    # Handle non int in list
+    if not_int_list(list_data) : raise ValueError("List contains non integer value")
+
     list_data.sort()
     # Create list consisting of upper half of list_data
     list_upperhalf = list_data[len(list_data)//2:]
@@ -109,6 +133,7 @@ def variance(list_data: list) -> float:
         Variance : Spread of numbers from the average value in the list
     :param list_data: list of values
     :return: float, variance of the list rounded to 3 decimals
+    Author: Tang.C
     '''
     # Handle non list input
     if not isinstance(list_data, list) : raise TypeError("Not list")
@@ -124,13 +149,14 @@ def variance(list_data: list) -> float:
         # Add difference squared to total
         total += abs(mean-num) * abs(mean-num)
     # Divide total by list length and round to 3 decimals
-    return round(total / len(list_data),3)
+    return round(total / len(list_data), 3)
 
 def standard_Deviation(list_data: list) -> float:
     ''' Returns standard deviation of list
         Standard Deviation: the square root of the spread of numbers from the average value in the list
     :param list_data: list of values
     :return: float, standard deviation of the list rounded to 3 decimals
+    Author: Ni.T
     '''
     if len(list_data) == 0: return -1
     mean = sum(list_data) / len(list_data)
